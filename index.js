8ce43943958976923d68f8f7fbd41ce56a203cba
TCG <ssibonokuhle@gmail.com>
2020-12-04 09:23:37 +0200

    added function

---
+++ index.js
@@ -0,0 +1,7 @@
+function calcSalary() {
+    var wage = parseFloat(document.getElementById('txt_wage').value);
+    var hours = parseFloat(document.getElementById('txt_hours').value);
+    var calculate = wage * hours * 52;
+    
+    document.getElementById('results').innerHTML = calculate;
+   }
